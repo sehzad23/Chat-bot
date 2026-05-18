be80b08e0fa5f9a494ebddc9b0f29f9ae6372374
sehzad23 <sehaans87@gmail.com>
2026-05-18 18:55:49 +0530

    Update CORS origin to production URL for socket.io

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173", // tera frontend port
+    origin: "https://your-chat-bot.vercel.app/", // tera frontend port
     methods: ["GET", "POST"],
   },
 });
